e20c8d44e320e23d26d7b38b46c16275fe05a000
jeffreylu0 <jljefflu@gmail.com>
2022-11-25 17:20:24 -0500

    added function to convert csv with annotated entities to .spacy format

--- utils/training_data.py
+++ utils/training_data.py
@@ -5,14 +5,15 @@ from pathlib import Path
 from typing import Union
 from spacy.tokens import DocBin
 
-def convert_labeled_csv_to_spacy(csv_path: Union[Path, str],
-                                 output_path: Union[Path, str] = './train.spacy') -> None:
+# Labels under 'Class' field
+def convert_textcat_csv_to_spacy(csv_path: Union[Path, str],
+                                 output_path: Union[Path, str] = './textcat_train.spacy') -> None:
     nlp = spacy.blank('en')
-    labeled_df = pd.read_csv(csv_path)
+    df = pd.read_csv(csv_path)
     db = DocBin()
 
-    training_data = [(portion,label) for portion,label in zip(labeled_df['Text Portion'], labeled_df['Class'])]
-    categories = labeled_df['Class'].unique()
+    training_data = [(portion,label) for portion,label in zip(df['Text Portion'], df['Class'])]
+    categories = df['Class'].unique()
 
     for text,label in training_data:
         doc = nlp(text)
@@ -22,6 +23,28 @@ def convert_labeled_csv_to_spacy(csv_path: Union[Path, str],
 
     db.to_disk(output_path)
 
+# Entity annotation under 'Entity', 'Start', and 'End' fields
+def convert_ner_csv_to_spacy(csv_path: Union[Path, str],
+                             output_path: Union[Path, str] = './ner_train.spacy') -> None:
+    nlp = spacy.blank('en')
+    df = pd.read_csv(csv_path)
+    db = DocBin()
+
+    training_data = [(portion,[(entity, start, end)]) 
+                     for portion, entity, start, end 
+                     in zip(df['Text Portion'], df['Entity'], df['Start'], df['End'])]
+    
+    for text, annotations in training_data:
+        doc = nlp(text)
+        ents = []
+        for entity, start, end in annotations:
+            span = doc.char_span(start, end, label=entity)
+            ents.append(span)
+        doc.ents = ents
+        db.add(doc)
+
+    db.to_disk(output_path)
+    
 if __name__ == '__main__':
-    convert_labeled_csv_to_spacy(sys.argv[1], sys.argv[2])
+    convert_textcat_csv_to_spacy(sys.argv[1], sys.argv[2])
 
